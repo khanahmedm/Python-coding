20700d15fa0fbccb9aa3929a12ef291799941828
Ahmed M Khan <khan.ahmed.m@outlook.com>
2023-04-22 10:32:18 -0500

    gets hours, count them and sort and reverse sort them by hours and counts

--- data-structures/hours_count.py
+++ data-structures/hours_count.py
@@ -13,13 +13,27 @@ for line in fh:
         
         counts[hour] = counts.get(hour, 0) + 1
 
-print('Sorted by keys')
+print('Sorted by hours (keys):')
 for key, val in sorted(counts.items()):
     print(key, val)
 
-#counts = sorted(counts.items, key = lambda x : x[1])
 print()
 
-print('Sorted by values')
+print('Sorted by counts (values):')
 for key, val in sorted(counts.items(), key = lambda x : x[1]):
     print(key, val)
+
+print()
+
+print('Reverse sorted by hours (keys):')
+for key, val in sorted(counts.items(), reverse = True):
+    print(key, val)
+
+print()
+
+print('Reverse sorted by counts (values):')
+for key, val in sorted(counts.items(), reverse = True, key = lambda x : x[1]):
+    print(key, val)
+
+
+
