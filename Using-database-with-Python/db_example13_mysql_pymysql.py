ed5228945708c11cc66f8393c2c40a128eadbb93
Ahmed M Khan <khan.ahmed.m@outlook.com>
2023-04-30 11:05:10 -0500

    connect to mysql using pymysql package

---
+++ Using-database-with-Python/db_example13_mysql_pymysql.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Install the following before running the code.
+# pip install PyMySQL
+
+import pymysql
+
+connection = pymysql.connect(
+        host = "<fill>",
+        user = "<fill>",
+        password = "<fill>",
+        db = "<fill>"
+    )
+
+cursor = connection.cursor()
+cursor.execute("select @@version")
+version = cursor.fetchone()
+
+if version:
+    print('Running version: ', version)
+else:
+    print('Not connected.')
+
+cursor = connection.cursor()
+cursor.execute("select * from country limit 10")
+
+row = cursor.fetchone()
+
+while row:
+    print(str(row[1]) + '  ' + str(row[3]))
+    row = cursor.fetchone()
+
+connection.close()
